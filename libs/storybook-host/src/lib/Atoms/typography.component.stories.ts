a9369b9c15e9a2aa821f73f5a3c96f1e1411da79
Christopher <christopher.gomes@goias.gov.br>
2025-11-18 08:13:21 -0300

    story dinamico

--- libs/storybook-host/src/lib/Atoms/typography.component.stories.ts
+++ libs/storybook-host/src/lib/Atoms/typography.component.stories.ts
@@ -1,24 +1,77 @@
 import { argsToTemplate, type Meta, type StoryObj } from '@storybook/angular';
 import { TypographyComponent } from '@goias-ssi/typography';
 
-const meta: Meta<TypographyComponent> = {
+type TypographyAndCustomArgs = TypographyComponent & { text: string }
+
+const meta: Meta<TypographyAndCustomArgs> = {
   component: TypographyComponent,
-  render: (args) => ({
+  args: {
+    text: 'Texto atualizado'
+  },
+  render: ({ text, ...args }) => ({
     props: args,
     template: `
     <go-typography ${argsToTemplate(args)}>
-      Text
+      ${text}
     </go-typography>
     `
-  })
+  }),
+  parameters: {
+    controls: {
+      include: ['variant', 'size', 'text']
+    }
+  }
 };
 
 export default meta;
 
-type Story = StoryObj<TypographyComponent>;
+type Story = StoryObj<TypographyAndCustomArgs>;
 
-export const Primary: Story = {
+export const Title1: Story = {
   args: {
-    variant: 'title3',
+    variant: "title1",
+    text: "Texto atualizado 123"
   },
+};
+
+export const Title2: Story = {
+  args: {
+    variant: "title2"
+  },
+};
+
+export const TextLarge: Story = {
+  args: {
+    variant: "normal"
+  },
+};
+
+export const TextMedium: Story = {
+  args: {
+    ...TextLarge.args,
+    size: 'md'
+  },
+};
+
+export const Title2WithMediumSize: Story = {
+  args: {
+    ...Title2.args,
+    size: 'md'
+  },
+};
+
+export const Paragraph: Story = {
+  args: {
+    ...TextLarge.args,
+  },
+  render: (args) => ({
+    props: args,
+    template: `
+    <p>
+      <go-typography ${argsToTemplate(args)}>
+        Novo conteúdo
+      </go-typography>
+    </p>
+    `
+  })
 };
--- libs/ui/typography/src/lib/typography/typography.component.ts
+++ libs/ui/typography/src/lib/typography/typography.component.ts
@@ -1,5 +1,6 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit, TemplateRef, ViewChild, ViewContainerRef } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { H1Component, H2Component, H3Component, H4Component, H5Component, Size, SpanComponent } from './dynamic.component';
 
 type Text =
   | 'title1'
@@ -15,6 +16,33 @@ type Text =
   templateUrl: './typography.component.html',
   styleUrl: './typography.component.css',
 })
-export class TypographyComponent {
+export class TypographyComponent implements OnInit {
   @Input() variant: Text = 'normal';
+  @Input() size!: Size;
+  component: any = SpanComponent;
+  private componentsMap = {
+    title1: H1Component,
+    title2: H2Component,
+    title3: H3Component,
+    subtitle1: H4Component,
+    subtitle2: H5Component,
+    normal: SpanComponent,
+  } satisfies { [key in Text]: any }
+  dynamicComponentContent!: any[][];
+  @ViewChild('template', { static: true }) template!: TemplateRef<unknown>;
+
+  constructor(private viewContainerRef: ViewContainerRef) { }
+
+  ngOnInit(): void {
+    this.component = this.componentsMap[this.variant];
+
+    const templateContent = this.viewContainerRef.createEmbeddedView(this.template)
+      .rootNodes;
+
+    this.dynamicComponentContent = [templateContent];
+  }
+
+  get inputs() {
+    return { size: this.size };
+  }
 }
