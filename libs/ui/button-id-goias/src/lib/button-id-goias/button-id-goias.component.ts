ca779e057f9b81f2201df1e5e22f796b9046979c
Christopher <christopher.gomes@goias.gov.br>
2025-11-20 10:00:13 -0300

    juntando componentes

--- libs/ui/button-id-goias/src/lib/button-id-goias/button-id-goias.component.ts
+++ libs/ui/button-id-goias/src/lib/button-id-goias/button-id-goias.component.ts
@@ -1,10 +1,14 @@
 import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { TypographyComponent } from '@goias-ssi/typography';
 
 @Component({
   selector: 'button-id-goias',
   standalone: true,
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    TypographyComponent,
+  ],
   templateUrl: './button-id-goias.component.html',
   styleUrl: './button-id-goias.component.css',
 })
